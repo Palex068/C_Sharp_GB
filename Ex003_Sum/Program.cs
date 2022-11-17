4b103bb5bf1e213d5b2201d767bd42fae232ccd4
Palex068 <palex068@gmail.com>
2022-11-17 09:05:53 +0300

    Add random to Ex003_Sum

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,10 @@
-﻿int numberA = 4;
-int numberB = 5;
+﻿//int numberA = 4; 
+int numberA = new Random().Next(1,10); //Добавим генератор случайных чисел
+//int numberB = 5;
+int numberB = new Random().Next(1,10); //Добавим генератор случайных чисел
 int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine("+");
+Console.WriteLine(numberB);
+Console.WriteLine("=");
 Console.WriteLine(result);
