582be57d36b94fb90c5b4531175c8299f081a9aa
Palex068 <palex068@gmail.com>
2022-11-19 15:14:49 +0300

    Ex007_Magic/ Ошибку нашёл)))

--- Ex007_Magic/Program.cs
+++ Ex007_Magic/Program.cs
@@ -22,22 +22,22 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while(count < 100);
+while(count < 1000)
 {
-    int what = new Random().Next(0,3);
-    if(what == 0)
+    int what = new Random().Next(0, 3); // [0;3) 0 1 2
+    if (what == 0)
     {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
     
-    if(what == 1)
+    if (what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
     
-    if(what == 2)
+    if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
@@ -45,5 +45,7 @@ while(count < 100);
 
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
-    count = count + 1; // count++ или count +=1
+    count++; // count++ или count +=1
+    // Console.SetCursorPosition(0, 0);
+    // Console.WriteLine(count);
 }
