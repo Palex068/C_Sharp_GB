520f4d23c6b4b56f7dbcdddb89642d4b0bcc65a0
Palex <palex068@gmail.com>
2022-11-19 13:24:05 +0300

    Change Ex005_ConditionIfElse
    Поправил комментарии о методе ToLower()

--- Ex005_ConditionIfElse/Program.cs
+++ Ex005_ConditionIfElse/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
-if(username.ToLower() == "Маша") // Добавили метод sting.ToLower() - возвращиет копию этой строки, переведенную в нижний регистр.
+if(username.ToLower() == "Маша")        // Добавили метод sting.ToLower() - возвращиет 
+                                        // копию этой строки, переведенную в нижний регистр.
 {
     Console.WriteLine("Ура, это же Маша!");
 }
