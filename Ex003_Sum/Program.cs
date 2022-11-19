ad060d37a2c9f57f9713cd0170e079857b2535e5
Palex <palex068@gmail.com>
2022-11-19 13:05:47 +0300

    Change Ex003_Sum
    Добавил описание генератора случайных чисел
    Добавил строки с пробелом в начале и конце для украшательства вывода работы программы

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,10 +1,15 @@
 ﻿//int numberA = 4; 
-int numberA = new Random().Next(1,10); //Добавим генератор случайных чисел
+int numberA = new Random().Next(1,10);  //Добавим генератор случайных чисел
+                                        //c выводом от 1 до 9 (включительно)
 //int numberB = 5;
-int numberB = new Random().Next(1,10); //Добавим генератор случайных чисел
+int numberB = new Random().Next(1,10);  //Добавим генератор случайных чисел
+                                        //c выводом от 1 до 9 (включительно)
 int result = numberA + numberB;
+
+Console.WriteLine(" ");
 Console.WriteLine(numberA);
 Console.WriteLine("+");
 Console.WriteLine(numberB);
 Console.WriteLine("=");
 Console.WriteLine(result);
+Console.WriteLine(" ");
