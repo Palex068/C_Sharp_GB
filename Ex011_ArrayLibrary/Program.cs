fe14c60e2ba6b657e62dc4b6fbc553d92eafbcea
Palex068 <palex068@gmail.com>
2022-11-22 22:35:45 +0300

    Ex011 Рассмотрели Методы: заполнения, печати Массива и Метод нахождения индекса значения

---
+++ Ex011_ArrayLibrary/Program.cs
@@ -0,0 +1,56 @@
+﻿
+Console.Clear();
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]}\t");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOf(array, 10);
+Console.WriteLine(pos);
+
+Console.WriteLine();
+
+
