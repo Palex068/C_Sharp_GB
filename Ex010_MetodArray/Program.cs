9026dc2f387bb4bc55d41f9e869f2059de751b6d
Palex068 <palex068@gmail.com>
2022-11-22 21:53:48 +0300

    Add Ex010_MetodArray Рассмотрели введение в Массивы и Методы
    Нахождение индекса известного числа в массиве, используя метод и массив

---
+++ Ex010_MetodArray/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.Clear();
+
+//               0   1   2  3   4   5   6   7   
+int[] array = { 11, 21, 31, 4, 18, 61, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index +1
+    index++;
+}
+Console.WriteLine();
